a96f5b3ddf6a6c66f5119eb2e9962964b869e37b
Pranshu Saurav <pranshusaurav04@gmail.com>
2018-02-22 23:32:53 +0530

    Added swipeRefreshLayout to activity_news_details files.

--- app/src/main/java/com/example/prans/news/NewsDetailsActivity.java
+++ app/src/main/java/com/example/prans/news/NewsDetailsActivity.java
@@ -1,17 +1,16 @@
 package com.example.prans.news;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ProgressBar;
 
 public class NewsDetailsActivity extends AppCompatActivity {
 
     WebView webView;
-    ProgressBar progressBar;
+    SwipeRefreshLayout refreshLayout;
     String url = "";
 
     @Override
@@ -20,25 +19,33 @@ public class NewsDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_news_details);
 
         url = getIntent().getStringExtra("url");
-
-        progressBar = findViewById(R.id.progressBar);
+        refreshLayout = findViewById(R.id.swipe_refresh);
+        refreshLayout.setRefreshing(true);
+        refreshLayout.setColorSchemeResources(R.color.colorAccent);
         webView = findViewById(R.id.webView);
 
         webView.getSettings().setBuiltInZoomControls(true);
         webView.getSettings().setDisplayZoomControls(false);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl(url);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                webView.loadUrl(url);
+            }
+        });
 
         webView.setWebChromeClient(new WebChromeClient() {
             public void onProgressChanged(WebView view, int progress) {
                 if (progress == 100) {
-                    progressBar.setVisibility(View.GONE);
+                    refreshLayout.setRefreshing(false);
                 } else {
-                    progressBar.setVisibility(View.VISIBLE);
+                    refreshLayout.setRefreshing(true);
                 }
             }
         });
     }
+
     @Override
     protected void onStop() {
         super.onStop();
